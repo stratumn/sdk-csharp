5d4bf81508ced11949769a5f7d6cf09bae88252d
Conor Dawson <conor.dawson@stratumn.com>
2020-07-10 10:53:28 +0200

    make sure the api endpoints are used (#15)

--- Sdk/Sdk/Helpers.cs
+++ Sdk/Sdk/Helpers.cs
@@ -27,10 +27,10 @@ namespace Stratumn.Sdk
         public static Endpoints MakeEndpoints(Endpoints endpoints)
         {
             if (endpoints == null)
-            {   
-                string ACCOUNT_RELEASE_URL = "https://account.stratumn.com";
-                string TRACE_RELEASE_URL = "https://trace.stratumn.com";
-                string MEDIA_RELEASE_URL = "https://media.stratumn.com";
+            {
+                string ACCOUNT_RELEASE_URL = "https://account-api.stratumn.com";
+                string TRACE_RELEASE_URL = "https://trace-api.stratumn.com";
+                string MEDIA_RELEASE_URL = "https://media-api.stratumn.com";
                 return new Endpoints(ACCOUNT_RELEASE_URL, TRACE_RELEASE_URL, MEDIA_RELEASE_URL);
             }
             if (endpoints.Account == null || endpoints.Trace == null || endpoints.Media == null)
