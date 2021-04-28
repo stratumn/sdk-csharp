99b19c42c2376bc2dfc27a76f39fa62161da7a4e
haroldcb <haroldcb@hotmail.fr>
2021-04-28 11:44:17 +0200

    Fix: Search without results do not throw an exception (#25)
    * Fix: Search without results do not throw an exception

--- Sdk/Sdk/Model/Trace/Info.cs
+++ Sdk/Sdk/Model/Trace/Info.cs
@@ -10,16 +10,6 @@
 
         public Info(bool hasNext, bool hasPrevious, string startCursor, string endCursor)
         {
-        
-            if (string.ReferenceEquals(startCursor, null))
-            {
-                throw new System.ArgumentException("startCursor cannot be null in Info");
-            }
-            if (string.ReferenceEquals(endCursor, null))
-            {
-                throw new System.ArgumentException("endCursor cannot be null in Info");
-            }
-
             this.HasNext = hasNext;
             this.HasPrevious = hasPrevious;
             this.StartCursor = startCursor;
